9d5f25e46a8d6690271464217c55459d4cfe57c8
Manellacanela <manuella.balbino@gmail.com>
2022-05-02 00:28:03 -0300

    git init

--- src/Nav.js
+++ src/Nav.js
@@ -1,19 +1,22 @@
 import React from 'react'
+const Nav = ({activeTab, onTabChange}) => {
 
-const Nav = () => {
-return (
-    <nav className= 'App-nav' >
-                 <ul>
-                     <li  className= 'App-nav-iten'> 
-                     <button>Itens</button>
-                     
-                     </li>
-                     <li className='App-nav-iten'>
-                         <button>Cart</button>
-                     </li>
-                 </ul>
-    </nav>
-)
+    const itemClass = (tabName) => `
+    App-nav-item ${
+        (activeTab === tabName ) ? 'selected' : ''}`
+
+    return (
+        <nav className="App-nav">
+            <ul>
+                <li className={itemClass('items')}>
+                    <button onClick={()=>onTabChange('items')}>Items</button>
+                </li>
+                <li className={itemClass('cart')}>
+                    <button onClick={()=> onTabChange('cart')}>Cart</button>
+                </li>
+            </ul>
+        </nav>
+    )
 }
 
 export default Nav;
